c879cbff09c3b2c82817d9f4a6861970dc52dd3c
Nathalia Martinez Vargas <64610064+NathaliaMartinezVargas@users.noreply.github.com>
2020-04-30 15:14:15 -0500

    Add files via upload

---
+++ Matrizcaracol.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Natalia
+#
+# Created:     30/04/2020
+# Copyright:   (c) Natalia 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    matriz = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+
+    print (matriz)
+
+    for i in range (3):
+        for j in range (3):
+            print (matriz[i][j])
+
+    print (matriz[0][0],matriz[0][1],matriz[0][2], matriz[1][2], matriz[2][2], matriz[2][1], matriz[2][0], matriz[1][0], matriz[1][1])
+
+
+
+
